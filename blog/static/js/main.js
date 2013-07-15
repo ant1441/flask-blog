8db50fc700a8b296003784d5c46ab3a54674096f
Adam <ant1441@gmail.com>
2013-07-15 23:35:36 +0100

    Add javascript for automatic slug generation.
    TODO: Avoid collisions with existing slugs.

---
+++ blog/static/js/main.js
@@ -0,0 +1,24 @@
+var slug = function (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+
+    // remove accents, swap ñ for n, etc
+    var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+    var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+};
+
+var setSlug = function() {
+    var slugTextBox = document.getElementById("slug");
+    var titleTextBox = document.getElementById("title");
+    var slugText = titleTextBox.value;
+    slugTextBox.value = slug(slugText);
+};
