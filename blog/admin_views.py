6fbe5edea9946b155b9dee5e399ff57ce82148b7
Adam <ant1441@gmail.com>
2013-07-21 16:27:56 +0100

    Modifications to Admin pages.
     * Added authentication to admin
     * Moved admin templates to local
     * Adjustments to templates
     * Added HTTP 500 template

--- blog/admin_views.py
+++ blog/admin_views.py
@@ -1,7 +1,17 @@
+from os.path import join
+from flask.ext.admin import BaseView, expose
 from flask.ext.admin.contrib.sqlamodel import ModelView
+from flask.ext.admin.contrib.fileadmin import FileAdmin
+from flask.ext.admin.base import MenuItem
 from flask.ext.login import current_user
 from blog import db, admin
-from models import User, Post, Category
+from blog.config import basedir
+from blog.models import User, Post, Category
+
+
+class AuthView(BaseView):
+    def is_accessible(self):
+        return current_user.is_authenticated()
 
 
 class DatabaseAdminView(ModelView):
@@ -9,7 +19,7 @@ class DatabaseAdminView(ModelView):
         return current_user.is_authenticated()
 
 
-class UserAdmin(ModelView):
+class UserAdmin(ModelView, AuthView):
     column_exclude_list = ['about_me', 'salt', 'password']
     column_sortable_list = ('username',
                             'email',
@@ -25,18 +35,28 @@ class UserAdmin(ModelView):
         super(UserAdmin, self).__init__(User, session)
 
 
-class PostAdmin(ModelView):
-    column_sortable_list = ('title', ('author', User.username), 'created_on')
+class PostAdmin(ModelView, AuthView):
+    column_sortable_list = ('title', ('author', User.username), 'created_at')
+    column_searchable_list = ('title', 'content')
 
     def __init__(self, session):
         super(PostAdmin, self).__init__(Post, session)
 
 
-class CategoryAdmin(ModelView):
-
+class CategoryAdmin(ModelView, AuthView):
     def __init__(self, session):
         super(CategoryAdmin, self).__init__(Category, session)
+
+
+class BlogFileAdmin(FileAdmin, AuthView):
+    def __init__(self, directory, url, *args, **kwargs):
+        super(BlogFileAdmin, self).__init__(join(basedir, directory),
+                                            url,
+                                            *args,
+                                            **kwargs)
+
 
 admin.add_view(UserAdmin(db.session))
 admin.add_view(PostAdmin(db.session))
 admin.add_view(CategoryAdmin(db.session))
+admin.add_view(BlogFileAdmin('static', '', name="Static Files"))
--- blog/error_views.py
+++ blog/error_views.py
@@ -7,4 +7,11 @@ def page_not_found(e):
     app.logger.warning("404 Error: %s from %s",
                        request.path,
                        request.remote_addr)
-    return render_template("404.html"), 404
+    return render_template("error/404.html"), 404
+
+app.errorhandler(500)
+def server_error(e):
+    app.logger.warning("500 Error: %s from %s",
+                       request.path,
+                       request.remote_addr)
+    return render_template("error/500.html"), 500
