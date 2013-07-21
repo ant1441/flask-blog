c9e448d45ecc43affc65f4e9a65fe1f2c39aceb1
Adam <ant1441@gmail.com>
2013-07-21 22:36:04 +0100

    Fixed setup.py file to include package data.

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from setuptools import setup
 
 setup(
     name="blog",
-    version=0.1,
+    version="0.1.1",
+    description="Simple blogging software.",
+    author="Adam Hodgen",
+    author_email="ant1441@gmail.com",
     packages=["blog"],
+    include_package_data=True,
 )
