fb0be37d3561973cf7eab223c4c623b28ae70317
Adam <ant1441@gmail.com>
2013-08-08 19:24:20 +0100

    Rearranged logging.

--- blog/models/__init__.py
+++ blog/models/__init__.py
@@ -1,6 +1,10 @@
+import logging
 from blog.models.post import Post
 from blog.models.user import User
 from blog.models.category import Category
+
+log = logging.getLogger(__name__)
+
 
 __all__ = [
     'Post',
